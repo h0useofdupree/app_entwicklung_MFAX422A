07d153b9aaa9d16f97353da18d20e9b8c4f19326
h0useofdupree <joel.riekemann@gmail.com>
2023-11-03 14:37:38 +0100

    add tts and exception handling

--- app/src/main/java/de/fhdw/app_entwicklung/chatgpt/MainFragment.java
+++ app/src/main/java/de/fhdw/app_entwicklung/chatgpt/MainFragment.java
@@ -1,16 +1,21 @@
 package de.fhdw.app_entwicklung.chatgpt;
 
 import android.os.Bundle;
+import android.service.controls.templates.TemperatureControlTemplate;
+import android.speech.tts.TextToSpeech;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import org.w3c.dom.Text;
 
 import java.util.Locale;
 
@@ -20,6 +25,8 @@ import de.fhdw.app_entwicklung.chatgpt.speech.LaunchSpeechRecognition;
 public class MainFragment extends Fragment {
 
     private static final String CHAT_SEPARATOR = "\n\n";
+    private static String answer;
+    private TextToSpeech tts;
 
     private final ActivityResultLauncher<LaunchSpeechRecognition.SpeechRecognitionArgs> getTextFromSpeech = registerForActivityResult(
             new LaunchSpeechRecognition(),
@@ -27,8 +34,8 @@ public class MainFragment extends Fragment {
                 getTextView().append(query);
 
                 MainActivity.backgroundExecutorService.execute(() -> {
-                    ChatGpt chatGpt = new ChatGpt("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");
-                    String answer = chatGpt.getChatCompletion(query);
+                    ChatGpt chatGpt = new ChatGpt("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");
+                    answer = chatGpt.getChatCompletion(query);
 
                     getTextView().append(CHAT_SEPARATOR);
                     getTextView().append(answer);
@@ -48,8 +55,30 @@ public class MainFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        getAskButton().setOnClickListener(v ->
-                getTextFromSpeech.launch(new LaunchSpeechRecognition.SpeechRecognitionArgs(Locale.GERMAN)));
+        getAskButton().setOnClickListener(v -> {
+            getTextFromSpeech.launch(new LaunchSpeechRecognition.SpeechRecognitionArgs(Locale.GERMAN));
+            Toast.makeText(requireContext(), "I'm Listening", Toast.LENGTH_SHORT).show();
+        });
+
+        getSpeakButton().setOnClickListener(v -> {
+            processTextToSpeech();
+        });
+
+        tts = new TextToSpeech(requireContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int result = tts.setLanguage(Locale.GERMAN);
+                    if (result == tts.LANG_MISSING_DATA || result == tts.LANG_NOT_SUPPORTED) {
+                        // handle ERROR
+                    } else {
+                        // SUCCESS
+                    }
+                } else {
+                    // INIT failed
+                }
+            }
+        });
     }
 
     private TextView getTextView() {
@@ -61,5 +90,20 @@ public class MainFragment extends Fragment {
         //noinspection ConstantConditions
         return getView().findViewById(R.id.button_ask);
     }
+
+    private Button getSpeakButton() {
+        return getView().findViewById(R.id.button_speak);
+    }
+    public void processTextToSpeech() {
+        if (answer == null) {
+            // Display a Toast message when the string is empty
+            Toast.makeText(requireContext(), "Ask me something first.", Toast.LENGTH_SHORT).show();
+        } else {
+            // Proceed with TextToSpeech processing if the string is not empty
+            // Your TextToSpeech code here
+            tts.speak(answer, TextToSpeech.QUEUE_FLUSH, null, null);
+        }
+    }
+
 
 }
